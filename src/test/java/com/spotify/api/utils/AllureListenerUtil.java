42b2cf0a319edf9d02d6f83fd66dcf7f41ee4334
gitstanhub <stasdmitruk1@gmail.com>
2023-05-03 03:49:16 +0200

    Updated the Allure Listener Util to fix the template path error

--- src/test/java/com/spotify/api/utils/AllureListenerUtil.java
+++ src/test/java/com/spotify/api/utils/AllureListenerUtil.java
@@ -7,8 +7,9 @@ public class AllureListenerUtil {
     private static final AllureRestAssured FILTER = new AllureRestAssured();
 
     public static AllureRestAssured customTemplates() {
-        FILTER.setRequestTemplate("tpl/request.ftl");
-        FILTER.setResponseTemplate("tpl/response.ftl");
+
+        FILTER.setRequestTemplate("request.ftl");
+        FILTER.setResponseTemplate("response.ftl");
         return FILTER;
     }
 }
