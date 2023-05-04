0a905a3c8b9b5adc5ec7766c1c2b283daa911375
gitstanhub <stasdmitruk1@gmail.com>
2023-05-04 02:19:58 +0200

    Added client and specs for Search endpoint

--- src/test/java/com/spotify/api/clients/SearchClient.java
+++ src/test/java/com/spotify/api/clients/SearchClient.java
@@ -1,4 +1,24 @@
 package com.spotify.api.clients;
 
+import com.spotify.api.utils.ApiAuthUtil;
+import io.restassured.response.ValidatableResponse;
+
+import static com.spotify.api.specifications.SearchSpec.searchRequestSpec;
+import static com.spotify.api.specifications.SearchSpec.searchResponseSpec;
+import static io.restassured.RestAssured.given;
+
 public class SearchClient {
+
+    public ValidatableResponse search() {
+        return given(searchRequestSpec)
+                .param("q","capital bra")
+                .param("type","album")
+        .when()
+                .get()
+        .then()
+                .spec(searchResponseSpec);
+    }
 }
+
+
+
