947f6d2ab41f2f2218babe5aa2b6b873c571a63c
gitstanhub <stasdmitruk1@gmail.com>
2023-05-05 03:04:50 +0200

    Updated the search method

--- src/test/java/com/spotify/api/clients/SearchClient.java
+++ src/test/java/com/spotify/api/clients/SearchClient.java
@@ -2,6 +2,9 @@ package com.spotify.api.clients;
 
 import com.spotify.api.utils.ApiAuthUtil;
 import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.*;
 
 import static com.spotify.api.specifications.SearchSpec.searchRequestSpec;
 import static com.spotify.api.specifications.SearchSpec.searchResponseSpec;
@@ -9,15 +12,31 @@ import static io.restassured.RestAssured.given;
 
 public class SearchClient {
 
-    public ValidatableResponse search() {
-        return given(searchRequestSpec)
-                .param("q","capital bra")
-                .param("type","album")
-        .when()
-                .get()
-        .then()
-                .spec(searchResponseSpec);
+    public ValidatableResponse search(String query, List<String> type, Optional<String> market, Optional<Integer> limit, Optional<Integer> offset, Optional<String> includeExternal) {
+        RequestSpecification request =
+                given(searchRequestSpec)
+                        .param("q", query)
+                        .param("type", type);
+
+        Map<String, Object> optionalParams = new HashMap<>();
+        optionalParams.put("market", market);
+        optionalParams.put("limit", limit);
+        optionalParams.put("offset", offset);
+        optionalParams.put("include_external", includeExternal);
+
+        optionalParams.forEach((key, value) -> {
+            if (value != null) {
+                request.param(key, value);
+            }
+        });
+
+        return request
+                .when()
+                        .get()
+                .then()
+                        .spec(searchResponseSpec);
     }
+
 }
 
 
--- src/test/java/com/spotify/api/tests/SearchTests.java
+++ src/test/java/com/spotify/api/tests/SearchTests.java
@@ -3,13 +3,25 @@ package com.spotify.api.tests;
 import com.spotify.api.clients.SearchClient;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class SearchTests {
 
     SearchClient searchClient = new SearchClient();
 
     @Test
     void searchTest() {
-        searchClient.search();
+        List<String> types = Arrays.asList("album");
+        Optional<String> marketEmpty = null;
+        Optional<Integer> limitEmpty = null;
+        Optional<Integer> offsetEmpty = null;
+        Optional<String> includeExternalEmpty = null;
+
+        searchClient.search("Capital Bra", types, marketEmpty, limitEmpty, offsetEmpty, includeExternalEmpty);
     }
 
 /// add test for regular album search
