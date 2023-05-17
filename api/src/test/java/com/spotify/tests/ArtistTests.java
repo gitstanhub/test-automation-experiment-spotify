9c51b875c3f31bedba04882a134eaaa9e7ab2892
gitstanhub <stasdmitruk1@gmail.com>
2023-05-17 04:11:51 +0200

    Removed unused code from artistProfileTest

--- api/src/test/java/com/spotify/tests/ArtistTests.java
+++ api/src/test/java/com/spotify/tests/ArtistTests.java
@@ -6,12 +6,10 @@ import com.spotify.models.response.artist.*;
 import com.spotify.utils.assertions.ApiAssertionsUtil;
 import com.spotify.utils.responsefields.artist.ArtistProfileFieldsUtil;
 import com.spotify.utils.responsefields.artist.ArtistTopTracksFieldsUtil;
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static com.spotify.testdata.artist.AristTracks.*;
 import static com.spotify.testdata.artist.ArtistAlbums.*;
@@ -75,38 +73,6 @@ public class ArtistTests {
 
 
         ArtistRelatedResponseModel artistRelated = artistClient.getRelatedArtists(CAPITAL_BRA_PROFILE.getArtistId());
-
-
-//        Map<Object, Object> artistDataValidationData = Map.of(
-//                artistResponseFieldsUtil.getArtistGenres(artistData), CAPITAL_BRA_PROFILE.getArtistGenres(),
-//                artistResponseFieldsUtil.getArtistName(artistData), CAPITAL_BRA_PROFILE.getArtistName(),
-//                artistResponseFieldsUtil.getArtistId(artistData), CAPITAL_BRA_PROFILE.getArtistId(),
-//                artistResponseFieldsUtil.getArtistType(artistData), CAPITAL_BRA_PROFILE.getArtistType(),
-//                artistResponseFieldsUtil.getArtistUri(artistData), CAPITAL_BRA_PROFILE.getArtistUri()
-//        );
-
-//        Map<Object, Object> artistTopTracksValidationData = Map.of(
-//                artistTopTracksFieldsUtil.getTrackName(artistTopTracks, CAPITAL_BRA_NEYMAR_TRACK.getTrackName()),CAPITAL_BRA_NEYMAR_TRACK.getTrackName(),
-//                artistTopTracksFieldsUtil.getTrackId(artistTopTracks, CAPITAL_BRA_NEYMAR_TRACK.getTrackName()),CAPITAL_BRA_NEYMAR_TRACK.getTrackId(),
-//                artistTopTracksFieldsUtil.getTrackDuration(artistTopTracks, CAPITAL_BRA_NEYMAR_TRACK.getTrackName()), CAPITAL_BRA_NEYMAR_TRACK.getTrackDurationMs(),
-//                artistTopTracksFieldsUtil.getTrackType(artistTopTracks, CAPITAL_BRA_NEYMAR_TRACK.getTrackName()), CAPITAL_BRA_NEYMAR_TRACK.getTrackType(),
-//                artistTopTracksFieldsUtil.getTrackAlbumName(artistTopTracks, CAPITAL_BRA_NEYMAR_TRACK.getTrackName()), CAPITAL_BRA_BERLIN_LEBT_ALBUM.getAlbumName(),
-//                artistTopTracksFieldsUtil.getTrackArtistName(artistTopTracks, CAPITAL_BRA_NEYMAR_TRACK.getTrackName(), CAPITAL_BRA_PROFILE.getArtistName()), CAPITAL_BRA_PROFILE.getArtistName(),
-//                artistTopTracksFieldsUtil.getTrackExplicitStatus(artistTopTracks, CAPITAL_BRA_NEYMAR_TRACK.getTrackName()), CAPITAL_BRA_NEYMAR_TRACK.getTrackExplicit()
-//        );
-
-//        Map<Object, Object> artistAlbumValidationData = Map.of(
-//
-//        );
-
-//        apiAssertionsUtil.verifyResponseMultipleFields(artistTopTracksValidationData);
-//        apiAssertionsUtil.verifyResponseMultipleFields(artistDataValidationData);
-
-//                artistResponseFieldsUtil.getArtistAlbumName(artistAlbums, 0), ARTIST_ALBUM_TITLE,
-//                artistResponseFieldsUtil.getArtistRelated(artistRelated, 0), ARTIST_RELATED_NAME
-
-
-
 
     }
 
