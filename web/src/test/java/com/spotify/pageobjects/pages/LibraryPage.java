393eb4d4ab9f754da57d89b01e1558e07d94b303
gitstanhub <stasdmitruk1@gmail.com>
2023-07-07 18:43:21 +0200

    Added steps and related methods for libraryCanBeSortedAlphabetically

--- web/src/test/java/com/spotify/pageobjects/pages/LibraryPage.java
+++ web/src/test/java/com/spotify/pageobjects/pages/LibraryPage.java
@@ -1,4 +1,55 @@
 package com.spotify.pageobjects.pages;
 
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.spotify.utils.BrowserActions;
+import com.spotify.utils.ElementActions;
+import com.spotify.utils.ElementChecks;
+import org.junit.jupiter.api.Assertions;
+
 public class LibraryPage {
+
+    private final BrowserActions browserActions;
+    private final ElementActions elementActions;
+    private final ElementChecks elementChecks;
+    private final Page page;
+
+    public LibraryPage(Page page) {
+        this.page = page;
+        this.browserActions = new BrowserActions(page);
+        this.elementActions = new ElementActions(page);
+        this.elementChecks = new ElementChecks(page);
+    }
+
+    public LibraryPage verifyLibraryButtonIsAvailable() {
+        Assertions.assertTrue(elementChecks.isElementVisible(findLibraryPageTitle()));
+        return this;
+    }
+
+    public LibraryPage clickExpandLibraryButton() {
+        findExpandLibraryButton().click();
+        return this;
+    }
+
+    public LibraryPage selectSortByOption(String sortByOption) {
+        findSortByButton().click();
+
+        elementActions
+                .findElementBySelector("div[id = context-menu] li button span ")
+                .filter(new Locator.FilterOptions().setHasText(sortByOption)).click();
+
+        return this;
+    }
+
+    private Locator findLibraryPageTitle() {
+        return elementActions.findElementByText("Your Library");
+    }
+
+    private Locator findExpandLibraryButton() {
+        return elementActions.findElementBySelector("button[aria-label='Enlarge Your Library']");
+    }
+
+    private Locator findSortByButton() {
+        return elementActions.findElementBySelector("button[aria-label='Sort by']");
+    }
 }
--- web/src/test/java/com/spotify/tests/LibraryTests.java
+++ web/src/test/java/com/spotify/tests/LibraryTests.java
@@ -1,12 +1,26 @@
 package com.spotify.tests;
 
-public class LibraryTests {
+import com.spotify.pageobjects.pages.HomePage;
+import com.spotify.pageobjects.pages.LibraryPage;
+import com.spotify.tests.base.WebPlaywrightTestBase;
+import org.junit.jupiter.api.Test;
+
+public class LibraryTests extends WebPlaywrightTestBase {
+
+    private final HomePage homePage = new HomePage(page);
+    private final LibraryPage libraryPage = new LibraryPage(page);
 
     //@Test
     //public void playlistFolderCanBeCreated
 
-    //@Test
-    //public void libraryCanBeSortedAlphabetically
+    @Test
+    public void libraryCanBeSortedAlphabetically() {
+        homePage.openHomePage();
+        libraryPage
+                .verifyLibraryButtonIsAvailable()
+                .clickExpandLibraryButton()
+                .selectSortByOption("Alphabetical");
+    }
 
     //@Test
     //public void artistCanBeAddedToLibrary
